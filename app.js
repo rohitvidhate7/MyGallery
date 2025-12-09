4bd3d8b9173b414e24502b6f3f76e745c0e871fc
Rohit Sandip Vidhate <138186608+rohitvidhate7@users.noreply.github.com>
2025-12-09 09:21:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const nextBtn = document.getElementById("nextBtn");
 let currentIndex = 0;
 
 // Open Lightbox
+
 items.forEach((img, index) => {
     img.addEventListener("click", () => {
         currentIndex = index;
